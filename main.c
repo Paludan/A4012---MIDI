0684d471619012196756ce7e6115c0253e4443bb
Tobias Morell <tmorel14@student.aau.dk>
2014-12-04 15:27:38 +0100

    Nu med doxygen og korrekte variable

--- main.c
+++ main.c
@@ -28,24 +28,24 @@ struct note{
 };
 typedef struct note note;
 
-struct song_data{
+struct songData{
   unsigned int tempo;
   mode mode;
 };
-typedef struct song_data data;
+typedef struct songData data;
 
 /*Prototypes*/
 void findNoteLength(double x, int *, int *);
-void print_note(note);
-int get_hex(FILE*, int[]);
-void fill_song_data(data*, int[], int);
-int count_notes(int[], int);
-void fill_note(int, note*);
+void printNote(note);
+int getHex(FILE*, int[]);
+void fillSongData(data*, int[], int);
+int countNotes(int[], int);
+void fillNote(int, note*);
 void printSongData(data);
 
 int main(int argc, const char *argv[]){
   /*Variables*/
-  int high, low, j, numbers_in_text = 0, notes, i = 0;
+  int j, numbersInText = 0, notes, i = 0;
   data data;
   FILE *f = fopen(argv[1],"r");
   int *hex = (int *) malloc(CHARS * sizeof(int));
@@ -55,11 +55,11 @@ int main(int argc, const char *argv[]){
   }
   
   /*Reading the data from the file*/
-  numbers_in_text = get_hex(f, hex);
-  fill_song_data(&data, hex, numbers_in_text);
-  notes = count_notes(hex, numbers_in_text);
-  note *note_ar = (note*) malloc(notes * sizeof(note));
-  if(note_ar == NULL){
+  numbersInText = getHex(f, hex);
+  fillSongData(&data, hex, numbersInText);
+  notes = countNotes(hex, numbersInText);
+  note *noteAr = (note*) malloc(notes * sizeof(note));
+  if(noteAr == NULL){
     printf("Memory allocation failed, bye!");
     exit(EXIT_FAILURE);
   }
@@ -67,7 +67,7 @@ int main(int argc, const char *argv[]){
   int note = 0x01, eventType = 0x01, counter = 0;
 
   /*Read and proces the hex array*/
-  for(j = 0; j < numbers_in_text; j++){
+  for(j = 0; j < numbersInText; j++){
     // Hops over any noto-on, note-off or metaevent start
     // Also stores the tones read after a note-on
     if(hex[j] == 0x00 && (hex[j + 1] == 0x90 || hex[j + 1] == 0xff)){
@@ -75,7 +75,7 @@ int main(int argc, const char *argv[]){
       j += 3;
       if(hex[j - 2] == 0x90){
         note = hex[j - 1];
-        fill_note(hex[j - 1], &note_ar[i]);
+        fillNote(hex[j - 1], &noteAr[i]);
         i++;
       }
       else{
@@ -97,35 +97,37 @@ int main(int argc, const char *argv[]){
   }
   
   for(i = 0; i < notes; i++)
-    print_note(note_ar[i]);
+    printNote(noteAr[i]);
   printSongData(data);
 
   /*Clean up and close*/
   fclose(f);
   free(hex);
-  free(note_ar);
+  free(noteAr);
 
   return 0;
 }
 
 /**A function, that retrieves the hexadecimals from the files and also returns the number of files
- *
-
-
- */
-int get_hex(FILE *f, int hex_ar[]){
+  *@param [FILE*] f: a pointer to the file the program is reading from
+  *@param [int] hexAr[]: an array of integers, that the information is stored in
+  */
+int getHex(FILE *f, int hexAr[]){
   int i = 0, c;
  
   while( (c = fgetc(f)) != EOF && i < CHARS){
-    hex_ar[i] = c;
+    hexAr[i] = c;
     i++;
   }
   
   return i;
 }
 
-/*A function to count the number of notes in the entire song*/
-int count_notes(int hex[], int ammount){
+/**A function to count the number of notes in the entire song
+  *@param [int] hex[]: an array with the stored information from the file
+  *@param [int] ammount: an integer holding the total number of characters in the array
+ */
+int countNotes(int hex[], int ammount){
   int i = 0, res = 0;
   for(i = 0; i < ammount; i++){
     if(hex[i] == 0x90){
@@ -135,12 +137,16 @@ int count_notes(int hex[], int ammount){
   return res;
 }
 
-/*A function, that fills out the song data*/
-void fill_song_data(data *data, int hex[], int numbers_in_text){
+/**A function, that fills out the song data
+  *@param [data*] data: a pointer to a structure containing the tempo and mode of the song
+  *@param [int] hex[]:the array of integers read from the file
+  *@param [int] numbersInText: the total ammount of integers in the array
+  */
+void fillSongData(data *data, int hex[], int numbersInText){
   int j;
   /*Find the mode of the song, initialised as minor atm*/
   data->mode = minor;
-  for(j = 0; j < numbers_in_text; j++){
+  for(j = 0; j < numbersInText; j++){
     /* finds the tempo */
     if(hex[j] == 0x51){
       data->tempo =  60000000/((hex[j+2] << 16) | (hex[j+3] << 8) | (hex[j+4]));
@@ -149,7 +155,8 @@ void fill_song_data(data *data, int hex[], int numbers_in_text){
 }
 
 
-/* Vi har ikke skalleret brøkken, Fix! */
+/**A function to calculate the notelenght - tba
+*/
 void findNoteLength (double x, int *high, int *low){
   double func = 16*((x*x)*(0.0000676318287050830)+(0.0128675448628599*x)-2.7216713227147);
   double temp = func;
@@ -164,14 +171,19 @@ void findNoteLength (double x, int *high, int *low){
   *low = 16;
 }
 
-/*Fills the note with calculations from the hex-array*/
-void fill_note(int i_tone, note *note){
-  note->tone = i_tone % 12;
-  note->octave = i_tone / 12;
+/**A function to fill out each of the structures of type note
+  *@param [int] iTone: the value of the hexadecimal collected on the "tone"-spot
+  *@param [note*] note: a pointer to a note-structure
+*/
+void fillNote(int iTone, note *note){
+  note->tone = iTone % 12;
+  note->octave = iTone / 12;
 }
 
-/*A function to print the note based on input*/
-void print_note(note note){
+/**A function to print the note
+  *@param [note] note: the note structure to be printed
+  */
+void printNote(note note){
   printf("Tone: ");
 
   switch (note.tone){
@@ -192,6 +204,9 @@ void print_note(note note){
   printf(", octave: %d\n", note.octave);
 }
 
+/**A function to print out the overall data of the song, tempo and mode
+  *@param [data] data: the data to be printed
+  */
 void printSongData(data data){
   printf("Tempo: %d\nMode: ", data.tempo);
   switch(data.mode){
