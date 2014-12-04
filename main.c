e554d007a4da28fae281e1805edc9ee259d2e829
Tobias Morell <tmorel14@student.aau.dk>
2014-12-04 13:35:46 +0100

    Med Jacobs sortering

--- main.c
+++ main.c
@@ -1,3 +1,14 @@
+/* MIDI analysing mega device of epic proportions
+ * Gruppe A412
+ * Lee Paludan
+ * Simon Madsen
+ * Jonas Stolberg
+ * Jacob Mortensen
+ * Esben Kirkegaard
+ * Arne Rasmussen
+ * Tobias Morell
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -25,11 +36,12 @@ typedef struct song_data data;
 
 /*Prototypes*/
 void findNoteLength(double x, int *, int *);
-void print_note(int);
+void print_note(note);
 int get_hex(FILE*, int[]);
 void fill_song_data(data*, int[], int);
 int count_notes(int[], int);
 void fill_note(int, note*);
+void printSongData(data);
 
 int main(int argc, const char *argv[]){
   /*Variables*/
@@ -41,41 +53,57 @@ int main(int argc, const char *argv[]){
     printf("Memory allokation failed, bye!");
     exit(EXIT_FAILURE);
   }
-  printf("Lee er et kæmpe fuckhoved\n");
+  
   /*Reading the data from the file*/
   numbers_in_text = get_hex(f, hex);
   fill_song_data(&data, hex, numbers_in_text);
   notes = count_notes(hex, numbers_in_text);
   note *note_ar = (note*) malloc(notes * sizeof(note));
-  
+  if(note_ar == NULL){
+    printf("Memory allocation failed, bye!");
+    exit(EXIT_FAILURE);
+  }
+  // Variables
+  int note = 0x01, eventType = 0x01;
+
   /*Read and proces the hex array*/
   for(j = 0; j < numbers_in_text; j++){
 
-    /* finder tonerne + oktav nr. */
-    if(hex[j] == 0x90){
-      fill_note(hex[1+j], &note_ar[i]);
-      printf("Oktav nr: %d\n", note_ar[i].octave);
-      printf("Tonen: ");
-      print_note(note_ar[i].tone);
-      putchar('\n');
-      i++;
+    // Hops over any noto-on, note-off or metaevent start
+    // Also stores the tones read after a note-on
+    if(hex[j] == 0x00 && (hex[j + 1] == 0x90 || hex[j + 1] == 0xff)){
+      j += 2;
+      if(hex[j - 1] == 0x90){
+	note = hex[j];
+        fill_note(hex[j], &note_ar[i]);
+        i++;
+      }
+      else
+        eventType = hex[j];
     }
-
-    /* finder tonelængden */
-    /*if(hex[j] == 0x90){
-      findNoteLength(hex[j+3], &high, &low);
-      printf("Nodelængde: %d/%d\n\n", high, low);
-    }*/
+    else if(hex[j] == 0x80 && hex[j + 1] == note){
+      j += 2;
+      note = 0x01;
+    }
   }
+  
+  for(i = 0; i < notes; i++)
+    print_note(note_ar[i]);
+  printSongData(data);
 
   /*Clean up and close*/
   fclose(f);
   free(hex);
+  free(note_ar);
 
   return 0;
 }
 
-/*A function, that retrieves the hexadecimals from the files and also returns the number of files*/
+/**A function, that retrieves the hexadecimals from the files and also returns the number of files
+ *
+
+
+ */
 int get_hex(FILE *f, int hex_ar[]){
   int i = 0, c;
  
@@ -87,6 +115,7 @@ int get_hex(FILE *f, int hex_ar[]){
   return i;
 }
 
+/*A function to count the number of notes in the entire song*/
 int count_notes(int hex[], int ammount){
   int i = 0, res = 0;
   for(i = 0; i < ammount; i++){
@@ -97,15 +126,15 @@ int count_notes(int hex[], int ammount){
   return res;
 }
 
+/*A function, that fills out the song data*/
 void fill_song_data(data *data, int hex[], int numbers_in_text){
   int j;
   /*Find the mode of the song, initialised as minor atm*/
   data->mode = minor;
   for(j = 0; j < numbers_in_text; j++){
-    /* finder tempoet */
+    /* finds the tempo */
     if(hex[j] == 0x51){
-      data->tempo =  60000000/((hex[j+2] << 16) | (hex[j+3] << 8) | (hex[j+4])); 
-      printf("Tempo: %d bpm\n\n", data->tempo);
+      data->tempo =  60000000/((hex[j+2] << 16) | (hex[j+3] << 8) | (hex[j+4]));
     }
   }
 }
@@ -126,17 +155,17 @@ void findNoteLength (double x, int *high, int *low){
   *low = 16;
 }
 
+/*Fills the note with calculations from the hex-array*/
 void fill_note(int i_tone, note *note){
   note->tone = i_tone % 12;
   note->octave = i_tone / 12;
-
 }
 
 /*A function to print the note based on input*/
-void print_note(int tone_number){
-  tone t = (tone) tone_number;
+void print_note(note note){
+  printf("Tone: ");
 
-  switch (t){
+  switch (note.tone){
     case C     : printf("C") ; break;
     case Csharp: printf("C#"); break;
     case D     : printf("D") ; break;
@@ -151,4 +180,15 @@ void print_note(int tone_number){
     case B     : printf("B") ; break;
     default    : printf("Undefined note"); break;
   }
+  printf(", octave: %d\n", note.octave);
+}
+
+void printSongData(data data){
+  printf("Tempo: %d\nMode: ", data.tempo);
+  switch(data.mode){
+    case minor: printf("minor"); break;
+    case major: printf("major"); break;
+    default: printf("unknown mode"); break;
+  }
+  putchar('\n');
 }
